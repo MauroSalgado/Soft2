2b30bfd48117d35d6917a23981e27cdcf6354734
MauroSalgado <30754450+MauroSalgado@users.noreply.github.com>
2018-08-20 20:46:57 -0500

    Tab Interests
    Creation of tab Interests

---
+++ Kpple/app/src/main/java/activities/TabInterests.java
@@ -0,0 +1,18 @@
+package activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.edu.konranlorenz.kpple.R;
+
+public class TabInterests extends android.support.v4.app.Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_interests, container, false);
+    }
+}
