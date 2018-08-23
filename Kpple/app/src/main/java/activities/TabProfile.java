95fff6b4c6d47553f58989d1de4c7d256dfc9eae
xhido <maok7g@hotmail.com>
2018-08-22 21:26:02 -0500

    Saving Profile
    Save profile fragment to Firebase

--- Kpple/app/src/main/java/activities/TabProfile.java
+++ Kpple/app/src/main/java/activities/TabProfile.java
@@ -2,24 +2,46 @@ package activities;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import co.edu.konranlorenz.kpple.R;
+import entities.User;
 
 public class TabProfile extends android.support.v4.app.Fragment implements View.OnClickListener{
-    TextInputEditText txtDate;
+
+    TextInputEditText txtInpName;
+    Spinner spnSex;
+    TextInputEditText txtBirthDate;
+    Spinner spnCountry;
+    Spinner spnCity;
+    Button btnSaveProfile;
+
+    FirebaseAuth mAuth;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab_profile, container, false);
-        txtDate = (TextInputEditText)view.findViewById(R.id.txtBirthDate);
-        txtDate.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        txtInpName = (TextInputEditText) view.findViewById(R.id.txtInpName);
+        spnSex = (Spinner) view.findViewById(R.id.spnSex);
+        spnCountry = (Spinner) view.findViewById(R.id.spnCountry);
+        spnCity = (Spinner) view.findViewById(R.id.spnCity);
+        btnSaveProfile = (Button) view.findViewById(R.id.btnSaveProfile);
+        btnSaveProfile.setOnClickListener(this);
+        txtBirthDate = (TextInputEditText)view.findViewById(R.id.txtBirthDate);
+        txtBirthDate.setOnClickListener(this);
         return view;
     }
 
@@ -29,7 +51,28 @@ public class TabProfile extends android.support.v4.app.Fragment implements View.
             case R.id.txtBirthDate:
                 showDatePickerDialog();
                 break;
+            case R.id.btnSaveProfile:
+                saveProfile();
         }
+    }
+
+    private void saveProfile() {
+        String name= txtInpName.getText().toString();
+        if(name.isEmpty()){
+            txtInpName.setError("Name Required");
+            txtInpName.requestFocus();
+            return;
+        }
+
+        FirebaseUser user = mAuth.getCurrentUser();
+        String id = user.getUid();
+        String sex = spnSex.getSelectedItem().toString();
+        String country = spnCountry.getSelectedItem().toString();
+        String city = spnCity.getSelectedItem().toString();
+        String birthdate = txtBirthDate.getText().toString();
+
+
+        User newUser = new User();
     }
 
     private void showDatePickerDialog() {
@@ -38,7 +81,7 @@ public class TabProfile extends android.support.v4.app.Fragment implements View.
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
                 final String selectedDate = day + " / " + (month+1) + " / " + year;
-                txtDate.setText(selectedDate);
+                txtBirthDate.setText(selectedDate);
             }
         });
         newFragment.show(getActivity().getFragmentManager(),"datePicker");
