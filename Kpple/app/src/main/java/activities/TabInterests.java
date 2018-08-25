e019c9acbc6eedfaee8c1114d02e27fea26b46fa
xhido <maok7g@hotmail.com>
2018-08-24 22:09:01 -0500

    Tab Interest
    Tab interest now can save data to firebase

--- Kpple/app/src/main/java/activities/TabInterests.java
+++ Kpple/app/src/main/java/activities/TabInterests.java
@@ -6,13 +6,70 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import co.edu.konranlorenz.kpple.R;
+import io.ghyeok.stickyswitch.widget.StickySwitch;
 
-public class TabInterests extends android.support.v4.app.Fragment{
+public class TabInterests extends android.support.v4.app.Fragment implements View.OnClickListener{
+
+    StickySwitch stcSwTrio;
+    StickySwitch stcSwVouyer;
+    StickySwitch stcSwSadism;
+    StickySwitch stcSwFetichism;
+    StickySwitch stcSwMasoquism;
+    StickySwitch stcSwSwinger;
+    Button btnSaveInterest;
+
+    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
+    private DatabaseReference refUser;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_interests, container, false);
+        View view = inflater.inflate(R.layout.tab_interests, container, false);
+        refUser = FirebaseDatabase.getInstance().getReference("Interest");
+        stcSwTrio = (StickySwitch) view.findViewById(R.id.stcSwTrio);
+        //stcSwTrio.setOnClickListener(this);
+        stcSwVouyer = (StickySwitch) view.findViewById(R.id.stcSwVouyer);
+        //stcSwVouyer.setOnClickListener(this);
+        stcSwSadism = (StickySwitch) view.findViewById(R.id.stcSwSadism);
+        //stcSwSadism.setOnClickListener(this);
+        stcSwFetichism = (StickySwitch) view.findViewById(R.id.stcSwFetichism);
+        //stcSwFetichism.setOnClickListener(this);
+        stcSwMasoquism = (StickySwitch) view.findViewById(R.id.stcSwMasoquism);
+        //stcSwMasoquism.setOnClickListener(this);
+        stcSwSwinger = (StickySwitch) view.findViewById(R.id.stcSwSwinger);
+        //stcSwSwinger.setOnClickListener(this);
+
+        btnSaveInterest = (Button) view.findViewById(R.id.btnSaveInterest);
+        btnSaveInterest.setOnClickListener(this);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btnSaveInterest:
+                FirebaseUser user = mAuth.getCurrentUser();
+
+                refUser.child("Trio").child(user.getUid()).setValue(stcSwTrio.getText());
+                refUser.child("Vouyerism").child(user.getUid()).setValue(stcSwVouyer.getText());
+                refUser.child("Sadism").child(user.getUid()).setValue(stcSwSadism.getText());
+                refUser.child("Fetichism").child(user.getUid()).setValue(stcSwFetichism.getText());
+                refUser.child("Masoquism").child(user.getUid()).setValue(stcSwMasoquism.getText());
+                refUser.child("Swinger").child(user.getUid()).setValue(stcSwSwinger.getText());
+
+                Toast.makeText(getContext(), "Interest Saved. Please continue to Hobbies", Toast.LENGTH_SHORT).show();
+                btnSaveInterest.setVisibility(View.INVISIBLE);
+                break;
+        }
     }
 }
