786817d137d9e76ca053388b0561fc736e6f7597
MauroSalgado <andresm.salgadon@konradlorenz.edu.co>
2018-10-02 23:53:51 -0500

    Chat Implementation
    Chat Scaledron

--- Kpple/app/src/main/java/activities/ActivityMessage.java
+++ Kpple/app/src/main/java/activities/ActivityMessage.java
@@ -1,25 +1,52 @@
 package activities;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
-public class ActivityMessage extends AppCompatActivity {
+import com.fasterxml.jackson.databind.JsonNode;
+import com.scaledrone.lib.Member;
+import com.scaledrone.lib.Room;
+import com.scaledrone.lib.RoomListener;
+import com.scaledrone.lib.Scaledrone;
 
-    private EditText editText;
+import co.edu.konranlorenz.kpple.R;
+
+public class ActivityMessage extends AppCompatActivity implements RoomListener {
+
+    private String channelID = "WaleAHsR6U7CZFlx";
+    private String roomName = "kpple_chat";
+    private EditText txtMessage;
+    private Scaledrone scaledrone;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message);
+        txtMessage = findViewById(R.id.txtMessage);
         getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     public void sendMessage(View view) {
-        String message = editText.getText().toString();
+        String message = txtMessage.getText().toString();
         if (message.length() > 0) {
-            editText.getText().clear();
+            txtMessage.getText().clear();
         }
+    }
+
+    @Override
+    public void onOpen(Room room) {
+        System.out.println("Conneted to room");
+    }
+
+    @Override
+    public void onOpenFailure(Room room, Exception ex) {
+        System.err.println(ex);
+    }
+
+    @Override
+    public void onMessage(Room room, JsonNode message, Member member) {
+
     }
 }
---
+++ Kpple/app/src/main/java/entities/Message.java
@@ -0,0 +1,60 @@
+package entities;
+
+public class Message {
+    private String message;
+    private String urlPhoto;
+    private String name;
+    private String profilePhoto;
+    private String type_message;
+
+    public Message() {
+    }
+
+    public Message(String message, String urlPhoto, String name, String profilePhoto, String type_message) {
+        this.message = message;
+        this.urlPhoto = urlPhoto;
+        this.name = name;
+        this.profilePhoto = profilePhoto;
+        this.type_message = type_message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUrlPhoto() {
+        return urlPhoto;
+    }
+
+    public void setUrlPhoto(String urlPhoto) {
+        this.urlPhoto = urlPhoto;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProfilePhoto() {
+        return profilePhoto;
+    }
+
+    public void setProfilePhoto(String profilePhoto) {
+        this.profilePhoto = profilePhoto;
+    }
+
+    public String getType_message() {
+        return type_message;
+    }
+
+    public void setType_message(String type_message) {
+        this.type_message = type_message;
+    }
+}
