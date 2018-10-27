2e0160596f76776ed4a32a428af25368764bff63
mjrodriguez0512 <mariaj.rodriguezy@konradlorenz.edu.co>
2018-10-27 01:54:39 -0500

    Notificacion de Boton Me gusta
    Notificacion de Boton Me gusta

--- Kpple/app/src/main/java/adapters/PostAdapter.java
+++ Kpple/app/src/main/java/adapters/PostAdapter.java
@@ -1,10 +1,19 @@
 package adapters;
 
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -23,12 +32,18 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.Date;
 import java.util.List;
 
+import activities.PostViewer;
+import activities.PostViewerFragment;
 import co.edu.konranlorenz.kpple.R;
 import entities.Post;
 
 public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ImageViewHolder> {
+
+    public static final int NOTIFI_ID =1001;
+    public static final String NOTIF_MENSAJE = "NOTIF_MENSAJE";
     private Context mContext;
     private List<Post> mPosts;
 
@@ -106,6 +121,7 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ImageViewHolde
                 refPost.child(user).child(postID).child("like").setValue(addLike);
                 holder.txtLike.setText(String.valueOf(addLike));
                 setData(mPosts);
+                sendNotification();
             }
         });
         holder.imgDislike.setOnClickListener(new View.OnClickListener() {
@@ -146,5 +162,49 @@ public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ImageViewHolde
             imgLike = itemView.findViewById(R.id.imgLike);
             imgDislike = itemView.findViewById(R.id.imgDislike);
         }
+    }
+
+    private void sendNotification(){
+        Intent intent = new Intent(mContext, PostViewerFragment.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        int icon = Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP ? R.mipmap.ic_launcher : R.mipmap.ic_launcher;
+        NotificationManager notificationManager =
+                (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        int m = (int) ((new Date().getTime() / 1000L) % Integer.MAX_VALUE);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "default";
+            NotificationChannel channel = new NotificationChannel(channelId, mContext.getString(R.string.notif_title), NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(mContext.getString(R.string.notif_body));
+            channel.enableLights(true);
+            channel.setLightColor(Color.BLUE);
+            channel.setSound(defaultSoundUri, null);
+            channel.setShowBadge(true);
+            notificationManager.createNotificationChannel(channel);
+            Notification notification = new Notification.Builder(mContext, channelId)
+                    .setContentTitle(mContext.getString(R.string.notif_title))
+                    .setContentText(mContext.getString(R.string.notif_body))
+                    .setSmallIcon(icon)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .build();
+            notificationManager.notify(m, notification);
+        } else {
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(mContext)
+                    .setSmallIcon(icon)
+                    .setContentTitle(mContext.getString(R.string.notif_title))
+                    .setContentText(mContext.getString(R.string.notif_body))
+                    .setAutoCancel(true)
+                    .setSound(defaultSoundUri)
+                    .setContentIntent(pendingIntent)
+                    .setLights(Color.BLUE, 3000, 3000);
+            notificationManager.notify(m, notificationBuilder.build());
+        }
+
+
+
     }
 }
